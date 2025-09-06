34a64805f613494a895c5b2e8a46ca175ddb9dce
Moreno Pradita <morenopradita013@gmail.com>
2025-09-06 11:46:17 +0700

    Part 2 - Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
@@ -31,13 +32,16 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.coffeeList,
-                R.id.favoritesFragment
+                R.id.favoritesFragment,
+                R.id.cafeFragment
             ),
             findViewById(R.id.drawer_layout)
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        findViewById<NavigationView>(R.id.nav_view)
-            ?.setupWithNavController(navController)
+
+        findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
